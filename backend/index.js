80dafd435652f89017a7cea2586ce4b137ce6011
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-21 20:51:52 +0300

    Api İstekleri İçin Hazırlık

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("irem")
+const express =require("express");
+const app = express();
+const cors=require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("",(req,res)=>{
+    res.json({message:"api isteği başarılı"});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>console.log("uygulama ayağa kalktı"));
