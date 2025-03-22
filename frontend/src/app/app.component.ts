d717ef20f814a0d97382aafaca6ad14a066ad316
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-22 23:52:37 +0300

    Layout Yapısı

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule],
+  standalone:true,
+  template:"<router-outlet></router-outlet>"
 })
 export class AppComponent {
-  title = 'frontend';
+  
 }
