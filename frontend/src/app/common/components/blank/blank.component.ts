790db25a1b847c03bb55f9c77bfdf2c246859f0b
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-29 21:26:19 +0300

    Generic Blank Component

---
+++ frontend/src/app/common/components/blank/blank.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-blank',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './blank.component.html',
+  styleUrl: './blank.component.css'
+})
+export class BlankComponent {
+  @Input() title: string = "";
+  @Input() sectionTitle: string = ""; 
+}
