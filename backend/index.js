ce6a63b5f3cc93d6eed166a671dd13a0d6f0ef2d
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-30 23:07:15 +0300

    Product Api'yi Dışarıya Açma

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const app = express();
-const cors=require("cors");
+const cors = require("cors");
 const connection = require("./database/db");
-
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const authRouter=require("./routers/auth.routher");
+const authRouter = require("./routers/auth.router");
 const categoryRouter = require("./routers/category.router");
+const productRouter = require("./routers/product.router");
 
-app.use("/api/auth",authRouter);
+app.use("/api/auth", authRouter);
 app.use("/api/categories",categoryRouter);
+app.use("/api/products", productRouter);
 
 connection();
 
 //mongodb+srv://evopartion:<db_password>@eticaretdb.skcno.mongodb.net/?retryWrites=true&w=majority&appName=ETicaretDb
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=>console.log("uygulama ayağa kalktı"));
-
+app.listen(port,()=> console.log("Uygulama http://localhost:5000 portunda ayağa kalktı!"));
