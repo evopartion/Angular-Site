9d1156b1bc8c23778d4a59b6ad2fa4b7fa7d566d
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-31 19:26:46 +0300

    Product Listeleme Sayfası Tasarım

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,27 +1,41 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes=[
+export const routes: Routes = [
     {
-        path:"login",
-        loadComponent:()=> import("./components/auth/components/login/login.component").then(c=>c.LoginComponent)
+        path: "login",
+        loadComponent: 
+            ()=> import("./components/auth/components/login/login.component")
+            .then(c=> c.LoginComponent)
     },
     {
-        path:"register",
-        loadComponent:()=> import("./components/auth/components/register/register.component").then(c=>c.RegisterComponent)
+        path: "register",
+        loadComponent: 
+            ()=> import("./components/auth/components/register/register.component")
+            .then(c=> c.RegisterComponent)
     },
     {
-        path:"",
-        loadComponent: ()=> import("./components/layouts/layouts.component").then(c=> c.LayoutsComponent),
-        children:[
+        path: "",
+        loadComponent: 
+            ()=> import("./components/layouts/layouts.component")
+            .then(c=> c.LayoutsComponent),
+        children: [
             {
-                path:"",
-                loadComponent:()=> import("./components/home/home.component").then(c=>c.HomeComponent)
+                path: "",
+                loadComponent: 
+                    ()=> import("./components/home/home.component")
+                    .then(c=> c.HomeComponent)
             },
             {
-                path:"categories",
-                loadComponent:
-                    ()=> import("./components/categories/categories.component").then
-                        (c=>c.CategoriesComponent)
+                path: "products",
+                loadComponent: 
+                    ()=> import("./components/products/components/products/products.component")
+                    .then(c=> c.ProductsComponent)
+            },
+            {
+                path: "categories",
+                loadComponent: 
+                    ()=> import("./components/categories/categories.component")
+                    .then(c=> c.CategoriesComponent)
             }
         ]
     }
