c0b9f55cce35442c473717542fd53d8ac5c103c6
evopartion <61056779+evopartion@users.noreply.github.com>
2025-04-01 19:39:46 +0300

    Basket Service

--- frontend/src/app/components/layouts/navbar/navbar.component.ts
+++ frontend/src/app/components/layouts/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../../common/shared/shared.module';
+import { BasketService } from '../../baskets/services/basket.service';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [SharedModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+constructor(
+  public _basket: BasketService
+){
+  this._basket.getCount();
+}
 }
